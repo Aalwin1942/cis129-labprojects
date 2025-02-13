5723e3163cbf1593aa982dd32bacbf2b20d0484e
Aalwin1942 <aalwin@mail.pima.edu>
2025-02-13 16:18:39 -0700

    Update cis129_lab02_shortStory.py
    short story updated in cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+#Author: Amber Alwin
+#CIS129 first Python module lab
+#Prints a short story
+print('As I travel from the North, I see a great many strangers.')
+print('Each person with their own origin and story.')
+print('Yet despite all the differences, we each pass upon the same trail.')
+print('Some may say it has been planned by fate.')
+print('But we will never know, for until it happens, the future does not exist.')
